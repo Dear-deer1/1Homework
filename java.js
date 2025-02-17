47139021cc51f589d00d23148f0e79b993b390fe
Dear-deer1 <alenazlobina112@gmail.com>
2025-02-17 09:12:24 +0300

    домашки 1-3

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+// первая домашка//
 let str = 'abcde';
 alert(str[0]); 
 alert(str[1]); 
@@ -13,3 +14,53 @@ let a = parseInt(prompt("Введите первое число:"));
             console.log("Совершеннолетний"); } 
             else {
             console.log("Несовершеннолетний"); }
+
+// вторая домашка //
+
+// номер 1 //
+let age = parseInt(prompt("Введите ваш возраст:"));
+if (age < 65) {
+    console.log("Вам ещё рано на пенсию");
+} else {
+    console.log("Поздравляем с пенсионным возрастом!");
+}
+
+// номер 2//
+let num1 = parseInt(prompt("Введите первое число:"));
+let num2 = parseInt(prompt("Введите второе число:"));
+
+if (num1 > num2) {
+    console.log("Первое число больше");
+} else if (num2 > num1) {
+    console.log("Второе число больше");
+} else {
+    console.log("Числа равны");
+}
+
+// номер 3//
+let num = parseInt(prompt("Введите число:"));
+
+if (num % 2 === 0) {
+    console.log("Это чётное число");
+} else {
+    console.log("Это нечётное число");
+}
+
+// домашка 3//
+
+
+let box = document.querySelectorAll(".clickbox");
+box.forEach(function(clickbox) {
+    clickbox.addEventListener("click", function() {
+        let currentColor = window.getComputedStyle(clickbox).backgroundColor;
+        if (currentColor === "rgb(176, 196, 222)") { 
+            clickbox.style.backgroundColor = "yellow";
+            clickbox.style.margin = "2vw";
+            console.log("Цвет изменён на жёлтый");
+        } else {
+            clickbox.style.backgroundColor = "lightsteelblue";
+            clickbox.style.margin = "1vw";
+            console.log("Цвет изменён на lightsteelblue");
+        }
+    });
+});
